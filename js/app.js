4da1b77d1698837ba0a100a7d7f99a71e9df3b4c
Karthik A <karthik.a@kongsbergdigital.com>
2023-09-14 23:06:32 +0530

    adding more display

--- js/app.js
+++ js/app.js
@@ -11,3 +11,23 @@ const observer = new IntersectionObserver((entries)) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+window.addEventListener('scroll', reveal)
+
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+
+}
